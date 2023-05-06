351d4269038317c0cc05659de87299d0e43168d3
Abhinay Lunawat <abhinaylunawat5993@gmail.com>
2023-05-06 17:57:55 +0530

    adding changes of Java package concepts , JS files with code.
    adding changes of Java package concepts , JS files with code.
    
    Co-Authored-By: Kunal Kushwaha <kunalkushwaha453@gmail.com>
    Co-Authored-By: Sivaprakash Desingu <10977339+sivaprakashDesingu@users.noreply.github.com>

---
+++ myJsProgs/myapp.js
@@ -0,0 +1,23 @@
+var funcRef=function()
+{
+console.log("GUVI Geek Technologies, Chennai");
+alert("GUVI Geek Technologies, Chennai");
+};
+
+var Calculator=function(x,y)
+{
+var sum=(x+y);
+var sub=(x-y);
+var mul=(x*y);
+var div=(x/y);
+console.log("sum          : ",sum);
+console.log("substraction : ",sub);
+console.log("product      : ",mul);
+console.log("Division     : ",div);
+};
+
+var varCheck=function(x,y)
+{
+var exp=(x && y);
+console.log("Status Val : ",exp);
+};
