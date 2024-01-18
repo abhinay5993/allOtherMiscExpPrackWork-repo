6df64b363cdaeb9569abb631858f65b093f0cee8
abhinay5993 <abhinaylunawat5993@gmail.com>
2024-01-18 23:48:02 +0530

    TestLeaf problem solving with Arrays & Strings for JS

--- PlaywrightLearnExpTS/week-1/04_stringAssignment.js
+++ PlaywrightLearnExpTS/week-1/04_stringAssignment.js
@@ -1,20 +1,42 @@
 /*
-Given a string s consisting of words and spaces, return the length of the last word in the string.
- 
 Example 1:
- 
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
 Input: s = "Hello World"
 Output: 5
 Explanation: The last word is "World" with length 5.
- 
-Example 2:
- 
+
 Input: s = "   fly me   to   the moon  "
 Output: 4
 Explanation: The last word is "moon" with length 4.
+*/
+
+function funcGetLastWordLength(impStrData)
+{
+    if(typeof impStrData=='string')
+    {
+    console.log("\nEntered input String : "+impStrData);
+    let lstOfWords=impStrData.trim().split(" ");
+    let strLastWord=lstOfWords[lstOfWords.length-1];
+    console.log("\nLast word of input String : "+strLastWord);
+    let lenOfLastWord=strLastWord.length;
+    console.log("\nLength of '"+strLastWord+"' String : "+lenOfLastWord);
+    return lenOfLastWord;
+    }
+    else
+    {
+       return "Please enter charecter sequence only!.";
+    }
+}
+
+console.log(funcGetLastWordLength("   fly me   to   the moon  "));
+console.log(funcGetLastWordLength("Hello World"));
+console.log(funcGetLastWordLength("30409x,cmlldsc skdk wlldm34&3 dsd"));
+console.log(funcGetLastWordLength(23423343));
+
+/*
 
 Example 3: 
-
 Write a function to check if two strings are anagrams.
 
  Input: isAnagram('listen', 'silent')
@@ -22,16 +44,40 @@ Write a function to check if two strings are anagrams.
  Input: isAnagram('hello', 'world') 
  Output: false
  Explanation: An anagram is when you mix up the letters of a word to make a new one, using all the letters.
- 
-*/
-/*
-If the given string and reverse string is same --> palindrome
 
+*/
+
+/*
+Example 3:
+If the given string and reverse string is same --> palindrome
 Objective: If the given string is palindrome
 
 hints:
-
 1) use the above logic to reverse the string
 2) if the reverse string === original string --> true else the false
-
 */
+function funcPalindromeChecker(impStrData)
+{
+    if(typeof impStrData=='string')
+    {
+       impStrData=impStrData.toLowerCase();
+       console.log("\nEntered input String : "+impStrData);
+       let temRevStr="";
+       for(let i=impStrData.length-1; i>=0; i--)
+       {
+        temRevStr+=impStrData[i];
+       }
+       console.log("Populated Reverse String : "+temRevStr);
+       return (temRevStr===impStrData)?true:false;
+    }
+    else
+    {
+       return "Please enter charecter sequence only!.";
+    }
+}
+
+console.log("is it a Palindrom : "+funcPalindromeChecker(1233.344));
+console.log("is it a Palindrom : "+funcPalindromeChecker("111"));
+console.log("is it a Palindrom : "+funcPalindromeChecker("madam"));
+console.log("is it a Palindrom : "+funcPalindromeChecker(111));
+console.log("is it a Palindrom : "+funcPalindromeChecker("caratLane"));
--- PlaywrightLearnExpTS/week-1/05_arrayAssignment.js
+++ PlaywrightLearnExpTS/week-1/05_arrayAssignment.js
@@ -1,10 +1,8 @@
 /*
 1) Move Zeroes:
- 
 Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
  
 Example 1:
- 
 Input: nums = [0,1,0,3,12]
 Output: [1,3,12,0,0]
 Example 2:
@@ -13,31 +11,18 @@ Input: nums = [0]
 Output: [0]
 
 2) Array intersection
- 
 Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
  
 Example 1:
- 
 Input: nums1 = [1,2,2,1], nums2 = [2,2]
 Output: [2]
  
 Example 2:
- 
 Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 Output: [9,4]
 Explanation: [4,9] is also accepted.
 
-3) Find the maximum and minimum:
-
-Given an integer array, find the maximum amd minimum elements in an array and return their indices.
-
-Example: 
-Input: nums = [34, 7, 21, 89, 54, 10, 91, 67]
-Output: Maximum Element: 91, Index: 6
-Minimum Element: 7, Index: 1
-
 4) Remove Duplicates:
-
 Given an integer array with duplicate elements as input, return a new array with duplicates 
 elements removed. The order of the elements in the resulting array should be same as the order
 in the original array.
@@ -50,7 +35,6 @@ Output: resultArray = [1, 2, 3, 4, 5, 6]
 /*
 
 1) Find the number of occurances.  
-
 const nums = [2,4,5,2,1,2];
 const k = 2
 // output >> 3
@@ -58,10 +42,54 @@ const k = 2
 hint: loop through the array and compare the k with array index value and if matches, increase the count
 
 2) Two Sum 
-
 const nums = [2,4,7,8,11,14]; // sort array !!
 const target = 18;
 
 return the indices that has matching target? 7+11 (2,4), 4+14 (1,5)
 
 */
+
+
+/**
+ * 3) Find the maximum and minimum:
+ * Given an integer array, find the maximum amd minimum elements in an array and return their indices.
+ * Example: 
+ * Input: nums = [34, 7, 21, 89, 54, 10, 91, 67]
+ * Output: Maximum Element: 91, Index: 6
+ * Minimum Element: 7, Index: 1
+ * 
+ */
+
+function findMaxMinElementsIndexPosition(inpArrayData)
+{
+    let outPutArray=[];
+    let maxNum=inpArrayData[0];
+    let minNum=inpArrayData[0];
+    for(let i = 0; i <inpArrayData.length; i++)
+    {
+        if(typeof inpArrayData[i]=='number')
+        {
+            if(inpArrayData[i]>maxNum)
+            {
+             maxNum=inpArrayData[i];
+             outPutArray.push(i);
+            }
+
+            if(inpArrayData[i]<minNum)
+            {
+            minNum=inpArrayData[i];
+            outPutArray.push(i);
+            }
+        }
+        else
+        {
+        return "Please enter numbers only!!."
+        }
+    }
+    console.log("Max Element : "+maxNum);
+    console.log("Min Element : "+minNum);
+    console.log(outPutArray);
+    return outPutArray;
+}
+
+findMaxMinElementsIndexPosition([34,7,21,89,54,10,91,67]);
