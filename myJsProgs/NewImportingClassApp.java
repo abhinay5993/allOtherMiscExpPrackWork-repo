37c02a152c0f6362c33c2e1c1e8a159773c563ad
abhinay5993 <abhinaylunawat5993@gmail.com>
2024-06-23 17:40:35 +0530

    removing all archived old projects.

--- myJsProgs/NewImportingClassApp.java
+++
@@ -1,9 +0,0 @@
-import Test.TestPackage;
-public class NewImportingClassApp
-{
-public static void main(String []args){
-TestPackage t=new TestPackage();
-t.Test();
-}
-
-}
--- myJsProgs/TestPackage.java
+++
@@ -1,9 +0,0 @@
-package Test;
-public class TestPackage{
-public static void main(String args[]){
-System.out.println("I am testing Package concept");
-}
-public void Test(){
-System.out.println("I am testing package concept");
-}
-}
